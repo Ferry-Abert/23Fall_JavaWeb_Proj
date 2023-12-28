a90bf0f4fb398ed0ea1bd29446777a11057beff2
fejixi <fejixi2@gmail.com>
2023-12-28 22:03:58 +0800

    jsp和样式

--- src/main/java/com/spring/service/ShoucangjiluService.java
+++ src/main/java/com/spring/service/ShoucangjiluService.java
@@ -2,3 +2,8 @@ package com.spring.service;
 
 import com.base.IServiceBase;
 import com.spring.entity.Shoucangjilu;
+/**
+ *  业务类
+ */
+public interface ShoucangjiluService extends IServiceBase<Shoucangjilu> {
+}
