41658c0c90c94edb72fd0818a4105c67634a8764
3257727558 <3257727558@qq.com>
2023-12-28 22:42:28 +0800

    jsp

--- src/main/java/util/StringUtil.java
+++ src/main/java/util/StringUtil.java
@@ -18,62 +18,7 @@ public class StringUtil {
     public static String join(String des , Object list){
         StringBuffer buffer = new StringBuffer();
 
-        if(list instanceof int[]) {
-            int[] var = (int[]) list;
-            for(int i=0;i<var.length;i++){
-                if(i>0){
-                    buffer.append(des);
-                }
-                buffer.append(var[i]);
-            }
-        }else if(list instanceof long[]) {
-            long[] var = (long[]) list;
-            for(int i=0;i<var.length;i++){
-                if(i>0){
-                    buffer.append(des);
-                }
-                buffer.append(var[i]);
-            }
-        }else if(list instanceof double[]) {
-            double[] var = (double[]) list;
-            for(int i=0;i<var.length;i++){
-                if(i>0){
-                    buffer.append(des);
-                }
-                buffer.append(var[i]);
-            }
-        }else if(list instanceof float[]) {
-            float[] var = (float[]) list;
-            for(int i=0;i<var.length;i++){
-                if(i>0){
-                    buffer.append(des);
-                }
-                buffer.append(var[i]);
-            }
-        }else if(list instanceof String[]) {
-            String[] var = (String[]) list;
-            for(int i=0;i<var.length;i++){
-                if(i>0){
-                    buffer.append(des);
-                }
-                buffer.append(var[i]);
-            }
-        }else if(list instanceof boolean[]) {
-            boolean[] var = (boolean[]) list;
-            for(int i=0;i<var.length;i++){
-                if(i>0){
-                    buffer.append(des);
-                }
-                buffer.append(var[i]?"true" : "false");
-            }
-        }else if(list instanceof List){
-            List var = (List) list;
-            for(int i=0;i<var.size();i++){
-                if(i>0){
-                    buffer.append(des);
-                }
-                buffer.append(var.get(i));
-            }
+
         }else if(list instanceof Map){
             Map var = (Map) list;
             Iterator entries = var.entrySet().iterator();
